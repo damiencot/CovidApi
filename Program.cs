1829762a44a1c46e0bae5ba8daf3e2bcd9cd97dc
damiencot <damien.cot@gmail.com>
2021-03-10 14:48:02 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CovidApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                args = Console.ReadLine().Split(' ');
+                Console.WriteLine(args[0]);
+            }
+        }
+    }
+}

fe00b1f931b8eca8f9af6c09901110a7f6468bee
damiencot <damien.cot@gmail.com>
2021-03-11 09:39:37 +0100

    recherche par departement unique erreure

--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ namespace CovidApi
         private const string GLOBAL_DATA_URL = "https://coronavirusapi-france.now.sh/FranceLiveGlobalData";
         private const string GLOBAL_DATA_TOKEN = "FranceGlobalLiveData";
 
-        private const string GLOBAL_BY_DEPARTEMENT_URL = "https://coronavirusapi-france.now.sh/LiveDataByDepartement";
+        private const string DATA_BY_DEPARTEMENT_URL = "https://coronavirusapi-france.now.sh/LiveDataByDepartement";
+        private const string DATA_BY_DEPARTEMENT_TOKEN = "LiveDataByDepartement";
+
         private const string ALL_DEPARTEMENTS_URL = "https://coronavirusapi-france.now.sh/AllLiveData";
         private const string ALL_DEPARTEMENTS_TOKEN = "allLiveFranceData";
 
@@ -38,7 +40,7 @@ namespace CovidApi
 
             printer = new Printer
             {
-                TableWidth = 100
+                TableWidth = 96
             };
 
             Console.WriteLine("Covid-19 France!");
@@ -56,6 +58,18 @@ namespace CovidApi
                     case "departements":
                         runAsync(ALL_DEPARTEMENTS_URL, ALL_DEPARTEMENTS_TOKEN).GetAwaiter().GetResult();
                         break;
+                    case "departement":
+                        if(args.Length == 0)
+                        {
+                            Console.WriteLine("Le nom du département n'a pas été spécifié");
+                            return;
+                        }
+                        var parameters = new Dictionary<string, string>
+                        {
+                            {"Departement", args[0].toTitleCase() }
+                        };
+                        runAsync(DATA_BY_DEPARTEMENT_URL, DATA_BY_DEPARTEMENT_TOKEN, parameters).GetAwaiter().GetResult();
+                        break;
                     case "exit":
                         Environment.Exit(0);
                         break;
@@ -65,20 +79,35 @@ namespace CovidApi
             }
         }
 
-        static async Task runAsync(string requestUri, string token)
+        static async Task runAsync(string requestUri, string token, Dictionary<string, string> parameters = null)
         {
-            var json = await getDataAsync(GLOBAL_DATA_URL);
-            var data = JObject.Parse(json).SelectToken(token).ToObject<List<Data>>();
+
+            var baseUri = requestUri;
+
+            if (parameters != null)
+            {
+                foreach(var parameter in parameters)
+                {
+                    baseUri += $"?{parameter.Key}={parameter.Value}";
+                }
+            }
+
+            var json = await getDataAsync(baseUri);
+            var datas = JObject.Parse(json).SelectToken(token).ToObject<List<Data>>();
 
 
-            switch (requestUri)
+            switch (baseUri)
             {
                 case GLOBAL_DATA_URL:
-                    printGlobalData(data.First());
+                    printGlobalData(datas.First());
                     break;
-                case GLOBAL_BY_DEPARTEMENT_URL:
+                case DATA_BY_DEPARTEMENT_URL:
+                    string departement = string.Empty;
+                    parameters.TryGetValue("Departement", out departement);
+                    printOneDepartementData(datas.First(), departement);
                     break;
                 case ALL_DEPARTEMENTS_URL:
+                    printDepartmentsData(datas);
                     break;
                 default:
                     Console.WriteLine("Erreur durant l'exécution");
@@ -87,19 +116,42 @@ namespace CovidApi
 
         }
 
-        static void printGlobalData(Data data)
+        private static void printOneDepartementData(Data data, string nom)
         {
-            Console.WriteLine("Voici les données globales connus pour la france");
+            if (data == null)
+            {
+                Console.WriteLine($"Aucun département n'a été trouvé avec le nom \"{nom}\"");
+            }
+            Console.WriteLine($"Voici les données globales connues à l'heure actuelle pour le département choisi : \"{nom}\"");
+
+
             printData(data);
         }
 
-        static void printData(Data data)
+        static void printGlobalData(Data data)
         {
+            Console.WriteLine("Voici les données globales connus pour la france");
+            printData(data, global: true);
+        }
+        static void printDepartmentsData(List<Data> datas)
+        {
+            Console.WriteLine($"Voici les données globales connus pour tous les departements");
+            foreach (var data in datas)
+            {
+                printData(data);
+            }
+        }
+
+        static void printData(Data data, bool global = false)
+        {
+            var localisation = global ? "Pays" : "Département";
             printer.Line();
-            printer.Row("Date", "Pays", "Décés", "Guéris", "Hospitalisés", "Réanimation");
+            printer.Row("Date", localisation, "Décés", "Guéris", "Hospitalisés", "Réanimation");
             printer.Line();
             printer.Row(data.date.ToString("dd/MM/yyyy"), data.nom, data.deces, data.gueris, data.hospitalises, data.reanimation);
             printer.Line();
         }
+
+
     }
 }
---
+++ Utils/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidApi.Utils
+{
+    public static class StringExtension
+    {
+
+        public static string toTitleCase(this string str)
+        {
+            str = str.Replace("\"", "").ToLower();
+            str = char.ToUpper(str[0]) + str.Substring(1);
+            return str;
+
+        }
+    }
+}
